fb6c275bbc131d83ce17a89e2fdf67b8e2d814a5
Velasquez <leonardo.velasquez@educa.madrid.org>
2021-11-11 08:22:18 +0100

    modificación 2

--- src/EstrucutrasDeControl/II/Ejercicio27.java
+++ src/EstrucutrasDeControl/II/Ejercicio27.java
@@ -3,6 +3,7 @@ package EstrucutrasDeControl.II;
 public class Ejercicio27 {
     public static void main(String[] args) {
         System.out.println("modificacion entornos");
+        System.out.println( "modificacion ejercicio 1. Crea una rama nueva en carpetaCasa, haz varios cambios y súbelos");
     }
 
 }
